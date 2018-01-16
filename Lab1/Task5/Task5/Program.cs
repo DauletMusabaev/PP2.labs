485fecca27a62a5c5bf2bb75b5e08d60180794e2
Zhandos Baurzhan <zhandos.baurzhan@gmail.com>
2018-01-16 23:36:28 +0600

    Task5 Rectangle

---
+++ Lab1/Task5/Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle s = new Rectangle();
+
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1/Task5/Task5/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Rectangle
+    {
+        public double width;
+        public double length;
+        public double area;
+        
+        public Rectangle()
+        {
+            width = 10;
+            length = 15;
+            findArea();
+        }
+
+        public void findArea()
+        {
+            area = length * width;
+        }
+
+        public override string ToString()
+        {
+            return "area = " + area;
+        }
+    }
+}
