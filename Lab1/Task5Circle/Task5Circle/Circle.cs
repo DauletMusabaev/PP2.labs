a4f5e7d2e2466a62cfb652ba7f7f4ae31c685f7f
Zhandos Baurzhan <zhandos.baurzhan@gmail.com>
2018-01-16 23:45:05 +0600

    Task5 Circle

---
+++ Lab1/Task5Circle/Task5Circle/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5Circle
+{
+    class Circle
+    {
+        public double radius;
+        public double area;
+
+        public Circle()
+        {
+            radius = 15;
+            findArea();
+        }
+
+        public void findArea()
+        {
+            area = Math.PI * radius * radius;
+        }
+
+        public override string ToString()
+        {
+            return "radius = " + radius + "\narea = " + area;
+        }
+    }
+}
---
+++ Lab1/Task5Circle/Task5Circle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Circle c = new Circle();
+
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
