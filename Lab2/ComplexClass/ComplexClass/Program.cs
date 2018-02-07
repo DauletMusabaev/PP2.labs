e087618b90902032e99a3bfd0783b468ff602dca
Zhandos Baurzhan <zhandos.baurzhan@gmail.com>
2018-02-07 21:36:47 +0600

    Complex

--- Lab2/ComplexClass/ComplexClass/Program.cs
+++ Lab2/ComplexClass/ComplexClass/Program.cs
@@ -36,6 +36,24 @@ namespace ComplexClass
                 l.Simple();
                 return l;
             }
+            public static Complex operator -(Complex j, Complex k)
+            {
+                Complex l = new Complex((j.x * k.y - k.x * j.y) / gcd(j.y, k.y), j.y * k.y / gcd(j.y,k.y));
+                l.Simple();
+                return l;
+            }
+            public static Complex operator *(Complex j, Complex k)
+            {
+                Complex l = new Complex(j.x * k.x, j.y * k.y);
+                l.Simple();
+                return l;
+            }
+            public static Complex operator /(Complex j, Complex k)
+            {
+                Complex l = new Complex(j.x * k.y, j.y * k.x);
+                l.Simple();
+                return l;
+            }
             public override string ToString()
             {
                 return x + "/" + y;
@@ -46,23 +64,29 @@ namespace ComplexClass
         {
             string s1 = Console.ReadLine();
             string s2 = Console.ReadLine();
-            string[] s11 = new string[2];
-            string[] s22 = new string[2];
-            s11 = s1.Split('/');
-            s22 = s2.Split('/');
-            int a1 = int.Parse(s11[0]);
-            int a2 = int.Parse(s11[1]);
-            int b1 = int.Parse(s22[0]);
-            int b2 = int.Parse(s22[1]);
+            string[] d1 = new string[2];
+            string[] d2= new string[2];
+
+            d1 = s1.Split('/');
+            d2 = s2.Split('/');
+
+            int a1 = int.Parse(d1[0]);
+            int a2 = int.Parse(d1[1]);
+            int b1 = int.Parse(d2[0]);
+            int b2 = int.Parse(d2[1]);
+
             Complex q = new Complex(a1, a2);
             Complex w = new Complex(b1, b2);
+
             Complex e = q + w;
-            /*
             Complex r = q - w;
             Complex t = q * w;
             Complex i = q / w;
-            */
-            Console.WriteLine(e);
+
+            Console.WriteLine("Sum = " + e);
+            Console.WriteLine("Substraction = " + r);
+            Console.WriteLine("Multiplication = " + t);
+            Console.WriteLine("Division = " + i);
             Console.ReadKey();
         }
     }
