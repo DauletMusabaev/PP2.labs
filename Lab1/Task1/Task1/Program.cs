48a9569da342d8bde56eb9a98d4d66b243df3ea8
Zhandos Baurzhan <zhandos.baurzhan@gmail.com>
2018-02-07 10:51:13 +0600

    Task1

---
+++ Lab1/Task1/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string[] arr = line.Split(' '); 
+            foreach (string s in arr)
+            {
+                int x = int.Parse(s); 
+                bool ok = true;
+                for (int i = 2; i * i <= x; i++)
+                {
+                    if (x % i == 0)
+                    {
+                        ok = false;
+                        break;
+                    }
+                }
+                if (ok & x != 1) 
+                {
+                    Console.WriteLine(x); 
+                }
+                ok = true;
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
